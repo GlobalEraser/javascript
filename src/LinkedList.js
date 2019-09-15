4e00f923287acdc8fd358f9feec559648a85f1ea
global <global@top.ge>
2019-09-15 23:40:28 +0400

    initial commit

---
+++ src/LinkedList.js
@@ -0,0 +1,25 @@
+
+
+class LinkedList{
+
+
+    push(e){
+
+    }
+
+    pop(){
+        return null;
+    }
+
+    unshift(e){
+
+    }
+
+    shift(){
+        return null;
+    }
+
+    forEach(func){
+
+    }
+}
